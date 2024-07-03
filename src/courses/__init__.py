9dce1ddf9f964b118f87f49e9e1951ddf03eb399
dheerajranbir420@gmail.com <dheerajranbir420@gmail.com>
2024-07-03 09:45:30 +0530

    Add init file

---
+++ src/courses/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["courses_list","courses_recommender"]
+
+from .courses_list import ds_course, web_course, android_course, ios_course, uiux_course, resume_videos, interview_videos
+from .courses_recommender import course_recommender
